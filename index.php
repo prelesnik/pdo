1bd1b580a317d1ede39c642e754dd50d60b618a6
Mike Prelesnik <mprelesnik@mail.greenriver.edu>
2019-02-11 13:10:11 -0500

    added prepared statements

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <?php
+
+require '/home/prelesn/config.php';
 //CONNECT TO DB
 try {
     //instantiate a db object
-    $dbh = new PDO("mysq:dbname=mprelesn_grc", "prelesn", "u5ny1Y6m:*BFP6");
+    $dbh = new PDO("mysql:dbname=mprelesn_grc", "prelesn", "u5ny1Y6m:*BFP6");
     echo "Connected to database!";
 }
 catch (PDOException $e)
 {
     echo $e->getMessage();
 }
+
+//Bind the parameters
+$type = 'snake';
+$name = 'Slitherin';
+$color = 'green';
+
+$statement ->bindParam(':type', $type, PDO::PARAM_STR);
+$statement ->bindParam(':name', $name, PDO::PARAM_STR);
+$statement ->bindParam(':color', $color, PDO::PARAM_STR);
+
+//execute
+$statement->execute();
